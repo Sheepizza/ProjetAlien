b4e98d525838a27738725a6b973324ec663312ae
ELcuicui1 <thibaut.boussemart2005@gmail.com>
2024-10-24 18:36:01 +0200

    Push Crouch

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,4 +1,5 @@
 using Mirror;
+using Mirror.BouncyCastle.Tls;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,12 +8,20 @@ public class PlayerController : NetworkBehaviour
 {
     public float Speed = 5f;
     public float JumpForce = 4f;
+    public float CrouchHeight = 1f;
+    public float StandHeight = 2f;
+    public Vector3 StandScale = new Vector3(1f, 1f, 1f);
+    public Vector3 CrouchScale = new Vector3(1f, 0.5f, 1f);
     Rigidbody _rb;
+    CapsuleCollider _cb;
+    Transform transPlayer;
 
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponentInParent<Rigidbody>();
+        _cb = GetComponentInParent<CapsuleCollider> ();
+        transPlayer = GetComponentInParent<Transform> ();
     }
 
     // Update is called once per frame
@@ -21,6 +30,16 @@ public class PlayerController : NetworkBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             _rb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            _cb.height = CrouchHeight;
+            transPlayer.transform.localScale = CrouchScale;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            _cb.height = StandHeight;
+            transPlayer.transform.localScale = StandScale;
         }
     }
 
