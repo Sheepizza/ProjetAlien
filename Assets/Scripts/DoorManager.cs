6339e465771e3b69eb82238b1d1400c036f8dd42
Sam <159584133+Sheepizza@users.noreply.github.com>
2024-11-01 15:31:46 +0100

    Mécanique Boutons-> Portes

--- Assets/Scripts/DoorManager.cs
+++ Assets/Scripts/DoorManager.cs
@@ -1,5 +1,7 @@
 using Mirror;
+using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DoorManager : NetworkBehaviour
@@ -7,22 +9,18 @@ public class DoorManager : NetworkBehaviour
     public DoorsDatas doorsDatas;
     Dictionary<string, string> ButtonsDoors;
 
-    uint _id;
+    [SyncVar]
+    GameObject _door;
 
     public override void OnStartLocalPlayer()
     {
-        _id = transform.parent.GetComponent<NetworkIdentity>().netId;
-
-        // Initialisation du dictionnaire une seule fois par joueur
         if (isServer && isLocalPlayer)
         {
             ButtonsDoors = doorsDatas.GetJ1Dictionary();
-            Debug.Log("Dictionnaire du Joueur 1 initialisé.");
         }
         else if (isLocalPlayer)
         {
             ButtonsDoors = doorsDatas.GetJ2Dictionary();
-            Debug.Log("Dictionnaire du Joueur 2 initialisé.");
         }
     }
 
@@ -31,30 +29,43 @@ public class DoorManager : NetworkBehaviour
 
         if (ButtonsDoors != null && ButtonsDoors.ContainsKey(key))
         {
-            Debug.Log(ButtonsDoors[key]);
+            _door = GameObject.Find(ButtonsDoors[key]);
+            bool _isButtonActive = GameObject.Find(key).GetComponent<IsActivate>().IsActive;
+            if (_door != null)
+            {
+                CmdChangeDoorPos(_door,_isButtonActive);
+                GameObject.Find(key).GetComponent<IsActivate>().IsActive = !_isButtonActive;
+            }
+        }
+    }
 
-            // Basculer l'état de la porte
-            GameObject door = GameObject.Find(ButtonsDoors[key]);
-            if (door != null)
-            {
-                MeshRenderer renderer = door.GetComponent<MeshRenderer>();
-                if (renderer != null)
-                {
-                    renderer.enabled = !renderer.enabled;
-                }
-                else
-                {
-                    Debug.LogWarning("Le MeshRenderer est introuvable pour l'objet : " + ButtonsDoors[key]);
-                }
-            }
-            else
-            {
-                Debug.LogWarning("La porte pour la clé spécifiée est introuvable : " + ButtonsDoors[key]);
-            }
-        }
-        else
+    [Command]
+    void CmdChangeDoorPos(GameObject _door, bool _isActive)
+    {
+        //_door.transform.position = _door.transform.position + Vector3.up * 3 * (_isActive ? -1 : 1);
+        RpcChangeDoorPos(_door, _isActive);
+    }
+
+    [ClientRpc]
+    void RpcChangeDoorPos(GameObject _door, bool _isActive)
+    {
+        //_door.transform.position = _door.transform.position + Vector3.up * 3 * (_isActive ? -1 : 1);
+        StartCoroutine(ChangeDoorPos(_door, _isActive));
+    }
+
+    IEnumerator ChangeDoorPos(GameObject _door, bool _isActive)
+    {
+        Vector3 _startPos = _door.transform.position;
+        float _elapsedTime = 0f;
+        int _direction = _isActive ? -1 : 1;
+
+        while (_elapsedTime < 1f)
         {
-            Debug.LogWarning("Clé introuvable dans le dictionnaire ou dictionnaire non initialisé.");
+            _door.transform.position = Vector3.Lerp(_startPos, _startPos + Vector3.up * 3 * _direction, _elapsedTime / 1);
+            _elapsedTime += Time.deltaTime;
+            yield return null;
         }
+
+        _door.transform.position = _startPos + Vector3.up * 3 * _direction;
     }
 }
