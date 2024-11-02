e16b354a2c80c2c42d6a59a89987af4c791cf343
ELcuicui1 <thibaut.boussemart2005@gmail.com>
2024-11-02 22:52:00 +0100

    miam le bon crouch

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,14 +4,15 @@ using Mirror.BouncyCastle.Bcpg.Sig;
 using Mirror.BouncyCastle.Tls;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.Animations;
 
 public class PlayerController : NetworkBehaviour
 {
+    public float t;
     public float Speed = 5f;
-    public float JumpForce = 0.5f;
     Rigidbody _rb;
     CapsuleCollider _cb;
     public CapsuleCollider _cbCrouch;
@@ -34,29 +35,28 @@ public class PlayerController : NetworkBehaviour
     {
         if (Input.GetKeyDown(KeyCode.LeftShift))  //accroupis
         {
-            camHold.transform.localPosition = camCrouch.transform.localPosition;
-            _cb.height = _cbCrouch.height;
+            camHold.transform.localPosition = Vector3.Lerp(camHold.transform.localPosition, camCrouch.transform.localPosition, t); //progressivement
+            _cb.height = _cbCrouch.height; //progressivement
             Speed = 2f;
             //ajout anim accroupis
         }
         else if (Input.GetKeyUp(KeyCode.LeftShift)) //debout
         {
-            
             RaycastHit hit;
-            if(Physics.Raycast(transform.position, transform.up, out hit, 10f))
+            if(Physics.Raycast(transform.position, transform.up, out hit, 2f)) //si il veut se relever mais qu'il touche un mur, il reste accroupit.
             {
                 camHold.transform.localPosition = camCrouch.transform.localPosition;
                 _cb.height = _cbCrouch.height;
             }
             else
             {
-                camHold.transform.localPosition = camStandPosition;
+                camHold.transform.localPosition = Vector3.Lerp(camHold.transform.localPosition, camStandPosition, t);
                 _cb.height = colliderStandHeight;
                 Speed = 5f;
                 //ajout anim debout
             }
         }
-        //si il veut se relever mais qu'il touche un mur, il reste accroupit.
+        
     }
 
     private void FixedUpdate()
@@ -64,5 +64,9 @@ public class PlayerController : NetworkBehaviour
             _rb.velocity = transform.right * Input.GetAxis("Horizontal") * Speed + 
             transform.up * _rb.velocity.y + 
             transform.forward * Input.GetAxis("Vertical") * Speed;
+    }
+    private void waitForSeconds(float seconds)
+    {
+
     }
 }
