9d4643eff47ff31bc75fb9fb391d6b0879086728
Sam <159584133+Sheepizza@users.noreply.github.com>
2024-12-06 11:28:35 +0100

    Crouch à finir

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -43,7 +43,7 @@ public class PlayerController : NetworkBehaviour
         else if (Input.GetKeyUp(KeyCode.LeftShift)) //debout
         {
             RaycastHit hit;
-            if(Physics.Raycast(transform.position, transform.up, out hit, 2f)) //si il veut se relever mais qu'il touche un mur, il reste accroupit.
+            if (Physics.Raycast(transform.position, transform.up, out hit, 2f)) //si il veutse relever mais qu'il touche un mur, il reste accroupit.
             {
                 camHold.transform.localPosition = camCrouch.transform.localPosition;
                 _cb.height = _cbCrouch.height;
@@ -56,14 +56,14 @@ public class PlayerController : NetworkBehaviour
                 //ajout anim debout
             }
         }
-        
+
     }
 
     private void FixedUpdate()
     {
-            _rb.velocity = transform.right * Input.GetAxis("Horizontal") * Speed + 
-            transform.up * _rb.velocity.y + 
-            transform.forward * Input.GetAxis("Vertical") * Speed;
+        _rb.velocity = transform.right * Input.GetAxis("Horizontal") * Speed +
+        transform.up * _rb.velocity.y +
+        transform.forward * Input.GetAxis("Vertical") * Speed;
     }
     private void waitForSeconds(float seconds)
     {
