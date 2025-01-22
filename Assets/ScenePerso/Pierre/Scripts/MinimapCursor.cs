de2053e005062cd99c4547d08ad6fe973e166627
Woassing <pierre-1998@hotmail.fr>
2025-01-22 08:45:30 +0100

    REprise

--- Assets/ScenePerso/Pierre/Scripts/MinimapCursor.cs
+++ Assets/ScenePerso/Pierre/Scripts/MinimapCursor.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Mirror;
+using Mirror.BouncyCastle.Tls;
 using Mirror.Examples.Basic;
 using UnityEngine;
 
@@ -13,42 +14,45 @@ public class MinimapCursor : NetworkBehaviour
     // Start is called before the first frame update
     public override void OnStartLocalPlayer()
     {
+        
         if (isServer && isLocalPlayer)
         {
             Cursor = GameObject.Find("CursorPlayer1");
             Follow = gameObject.GetComponent<Transform>();
         }
-        else if (isLocalPlayer)
+        else if (isLocalPlayer && !isServer)
         {
             Cursor =  GameObject.Find("CursorPlayer2");
             Follow = gameObject.GetComponent<Transform>();
         }
+        //Cursor.GetComponent<NetworkIdentity>().AssignClientAuthority(connectionToClient);
     }
 
     // Update is called once per frame
     public void FixedUpdate()
     {
+        
         ChangeCursorPos();
     }
 
-    //[Command]
+    [Command(requiresAuthority = false)]
     void ChangeCursorPos()
     {   
-        if(Cursor != null)
+        /*if(Cursor != null)
         {
 
             Cursor.transform.position = new Vector3(Follow.transform.position.x,8,Follow.transform.position.z);
             Debug.Log("Cursor Pos : " + Cursor.transform.position);
-        }
-        /*Debug.Log("Commande lancée");
-        RpcChangeCursorPos();*/
+        }*/
+        Debug.Log("Commande lancée");
+        RpcChangeCursorPos();
     }
 
-    /*[ClientRpc]
+    [ClientRpc]
     void RpcChangeCursorPos()
     {
         Debug.Log("Commande envoyée");
         if (Cursor != null)
-            Cursor.transform.position = new Vector3(gameObject.transform.position.x,8,gameObject.transform.position.z);
-    }*/
+            Cursor.transform.position = new Vector3(Follow.transform.position.x,8,Follow.transform.position.z);
+    }
 }
