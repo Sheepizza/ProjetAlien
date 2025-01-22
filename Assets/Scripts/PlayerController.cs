592afb5225a61bcf5acd64909bcffa6571ddce8d
ELcuicui1 <thibaut.boussemart2005@gmail.com>
2025-01-22 11:18:43 +0100

    Sprint et Crouch propre

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,99 +9,133 @@ using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEngine.Animations;
+using Mirror.Examples.Billiards;
 
 public class PlayerController : NetworkBehaviour
 {
-    public float secondsRest;
-    public float secondsEffort;
-    public float t;
-    public float Speed = 5f;
     Rigidbody _rb;
     CapsuleCollider _cb;
+
+    [Header ("Speed")]
+    public float moveSpeed;
+    public float walkSpeed;
+    public float crouchSpeed;
+    public float sprintSpeed;
+
+    [Space(25)]
+
+    [Header ("Run")]
+    public float runningTime;
+    public float maxRunningTime;
+    public float minRunningTime;
+
+    [Space(25)]
+
+    [Header ("Crouch")]
+    public float t;
+    public float crouchYScale;
+    private float startYScale;
     public CapsuleCollider _cbCrouch;
     public GameObject camCrouch;
     public GameObject camHold;
-    public Slider enduranceSlider;
     Vector3 camStandPosition;
     float colliderStandHeight;
 
-    // Start is called before the first frame update
+    [Space(25)]
+
+    [Header ("KeyCode")]
+    public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode sprintKey = KeyCode.LeftShift;
+    public KeyCode crouchKey = KeyCode.LeftControl;
+
+    public bool EnSprint;
+
     void Start()
     {
+        moveSpeed = walkSpeed;
+        maxRunningTime = runningTime;
         _rb = GetComponentInParent<Rigidbody>();
         _cb = GetComponentInParent<CapsuleCollider>();
         camStandPosition = camHold.transform.localPosition;
         colliderStandHeight = _cb.height;
+        startYScale = transform.localScale.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftShift))  //accroupis
+        if (Input.GetKeyDown(crouchKey))  //accroupis
         {
+            transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
+            _rb.AddForce(Vector3.down * 2f, ForceMode.Impulse);
             camHold.transform.localPosition = Vector3.Lerp(camHold.transform.localPosition, camCrouch.transform.localPosition, t); //progressivement
             _cb.height = _cbCrouch.height; //progressivement
-            Speed = 2f;
+            moveSpeed = crouchSpeed;
             //ajout anim accroupis
         }
-        else if (Input.GetKeyUp(KeyCode.LeftShift)) //debout
+        else if (Input.GetKeyUp(crouchKey)) //debout
         {
             RaycastHit hit;
             if (Physics.Raycast(transform.position, transform.up, out hit, 2f)) //si il veutse relever mais qu'il touche un mur, il reste accroupit.
             {
                 camHold.transform.localPosition = camCrouch.transform.localPosition;
                 _cb.height = _cbCrouch.height;
+                moveSpeed = crouchSpeed;
             }
             else
             {
+                transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
                 camHold.transform.localPosition = Vector3.Lerp(camHold.transform.localPosition, camStandPosition, t);
                 _cb.height = colliderStandHeight;
-                Speed = 5f;
+                moveSpeed = walkSpeed;
                 //ajout anim debout
             }
         }
 
         //courir
-        if (Input.GetKeyDown(KeyCode.LeftControl))
+        if (Input.GetKeyDown(sprintKey) && moveSpeed == walkSpeed)
         {
-            Speed = 8f;
-            //diminuer la barre d'endurance
-            StartCoroutine(SliderEffort());
+            StartCoroutine(Course());
         }
-        else if(Input.GetKeyUp(KeyCode.LeftControl))
+        if (Input.GetKeyUp(sprintKey))
         {
-            Speed = 5f;
-            //recharger la barre d'endurance
-            StartCoroutine(SliderRest());
+            StopCoroutine(Course());
+            moveSpeed = walkSpeed;
+            EnSprint = false;
+            StartCoroutine(RechargementSprint());
         }
-
     }
 
     private void FixedUpdate()
     {
-        _rb.velocity = transform.right * Input.GetAxis("Horizontal") * Speed +
+        _rb.velocity = transform.right * Input.GetAxis("Horizontal") * moveSpeed +
         transform.up * _rb.velocity.y +
-        transform.forward * Input.GetAxis("Vertical") * Speed;
+        transform.forward * Input.GetAxis("Vertical") * moveSpeed;
     }
-
-    //pour régenérer le slider d'endurance
-    IEnumerator SliderRest()
+    IEnumerator Course() 
     {
-        while (enduranceSlider.value != 1)
+        EnSprint = true;
         {
-            yield return new WaitForSeconds(secondsRest);
-            enduranceSlider.value += 0.01f;
+            while(EnSprint)
+            {
+                moveSpeed = sprintSpeed;
+                runningTime -= Time.deltaTime;
+                if (runningTime <= minRunningTime)
+                {
+                    moveSpeed = walkSpeed;
+                    EnSprint = false;
+                }
+                Debug.Log("EnSprint");
+                yield return null;
+            }
         }
     }
-
-    IEnumerator SliderEffort()
+    IEnumerator RechargementSprint()
     {
-        while (enduranceSlider.value != 0)
+        while(moveSpeed == walkSpeed && runningTime < maxRunningTime)
         {
-            yield return new WaitForSeconds(secondsEffort);
-            enduranceSlider.value -= 0.01f;
+            runningTime += Time.deltaTime;
+            yield return null;
         }
     }
-
-
 }
