d4baee46edee59acd485fecd46e57f5b5c8fd58b
Sam <159584133+Sheepizza@users.noreply.github.com>
2025-04-23 10:24:39 +0200

    Prêt à build V0.0001

--- Assets/ScenePerso/ThibauLLLT/Script/RepairPanel.cs
+++ Assets/ScenePerso/ThibauLLLT/Script/RepairPanel.cs
@@ -1,152 +1,152 @@
-using System.Collections;
-using System.Collections.Generic;
-using Mirror.Examples.Common;
-using UnityEngine;
-using Mirror;
-using TMPro;
-using UnityEngine.UI;
+//using System.Collections;
+//using System.Collections.Generic;
+//using Mirror.Examples.Common;
+//using UnityEngine;
+//using Mirror;
+//using TMPro;
+//using UnityEngine.UI;
 
 
-public class RepairPanel : NetworkBehaviour
-{
-    public float pickUpRange = 2f;
-    public string[] brokenTags;
-    private Camera playerCamera;
-    private GameObject highlightedObject = null;
-    private GameObject repairText;
-    private GameObject repairMiniGamePanel;
-    private PlayerController playerController;
-    private PlayerCamera playerCameraScript;
-    public MiniGameController miniGame;
+//public class RepairPanel : NetworkBehaviour
+//{
+//    public float pickUpRange = 2f;
+//    public string[] brokenTags;
+//    private Camera playerCamera;
+//    private GameObject highlightedObject = null;
+//    private GameObject repairText;
+//    private GameObject repairMiniGamePanel;
+//    private PlayerController playerController;
+//    private PlayerCamera playerCameraScript;
+//    public MiniGameController miniGame;
 
-    void Start()
-    {
-        if (isLocalPlayer)
-        {
-            playerCamera = GameObject.Find("PlayerCamera").GetComponent<Camera>();
-            playerCameraScript = playerCamera.GetComponent<PlayerCamera>(); 
+//    void Start()
+//    {
+//        if (isLocalPlayer)
+//        {
+//            playerCamera = GameObject.Find("PlayerCamera").GetComponent<Camera>();
+//            playerCameraScript = playerCamera.GetComponent<PlayerCamera>(); 
 
-            GameObject uiManager = GameObject.Find("UIManager");
-            if (uiManager != null)
-            {
-                Canvas canvas = uiManager.GetComponentInChildren<Canvas>();
-                if (canvas != null)
-                {
-                    repairText = canvas.transform.Find("RepairText")?.gameObject;
-                    repairMiniGamePanel = canvas.transform.Find("RepairMiniGame")?.gameObject;
+//            GameObject uiManager = GameObject.Find("UIManager");
+//            if (uiManager != null)
+//            {
+//                Canvas canvas = uiManager.GetComponentInChildren<Canvas>();
+//                if (canvas != null)
+//                {
+//                    repairText = canvas.transform.Find("RepairText")?.gameObject;
+//                    repairMiniGamePanel = canvas.transform.Find("RepairMiniGame")?.gameObject;
                     
 
-                    if (repairText != null)
-                    {
-                        repairText.SetActive(false);
-                    }
+//                    if (repairText != null)
+//                    {
+//                        repairText.SetActive(false);
+//                    }
 
-                    if (repairMiniGamePanel != null)
-                    {
-                        repairMiniGamePanel.SetActive(false);
-                    }
-                }
-            }
+//                    if (repairMiniGamePanel != null)
+//                    {
+//                        repairMiniGamePanel.SetActive(false);
+//                    }
+//                }
+//            }
 
-            playerController = GetComponent<PlayerController>();
-        }
-    }
+//            playerController = GetComponent<PlayerController>();
+//        }
+//    }
 
-    void Update()
-    {
-        if (!isLocalPlayer) return;
+//    void Update()
+//    {
+//        if (!isLocalPlayer) return;
 
-        Ray ray = playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
-        RaycastHit hit;
+//        Ray ray = playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+//        RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit, pickUpRange))
-        {
-            GameObject targetObject = hit.collider.gameObject;
-            if (IsBrokenObject(targetObject))
-            {
-                if (repairText != null) repairText.SetActive(true);
+//        if (Physics.Raycast(ray, out hit, pickUpRange))
+//        {
+//            GameObject targetObject = hit.collider.gameObject;
+//            if (IsBrokenObject(targetObject))
+//            {
+//                if (repairText != null) repairText.SetActive(true);
 
-                HighlightObject(targetObject);
+//                HighlightObject(targetObject);
 
-                if (Input.GetKeyDown(KeyCode.E))
-                {
-                    //OpenRepairPanel();
-                }
-            }
-            else
-            {
-                if (repairText != null) repairText.SetActive(false);
-                if (highlightedObject != null)
-                {
-                    var outlineComponent = highlightedObject.GetComponent<Outline>();
-                    if (outlineComponent != null)
-                    {
-                        outlineComponent.enabled = false;
-                    }
-                    highlightedObject = null;
-                }
-            }
-        }
-        else
-        {
-            if (repairText != null) repairText.SetActive(false);
-            if (highlightedObject != null)
-            {
-                var outlineComponent = highlightedObject.GetComponent<Outline>();
-                if (outlineComponent != null)
-                {
-                    outlineComponent.enabled = false;
-                }
-                highlightedObject = null;
-            }
-        }
+//                if (Input.GetKeyDown(KeyCode.E))
+//                {
+//                    //OpenRepairPanel();
+//                }
+//            }
+//            else
+//            {
+//                if (repairText != null) repairText.SetActive(false);
+//                if (highlightedObject != null)
+//                {
+//                    var outlineComponent = highlightedObject.GetComponent<Outline>();
+//                    if (outlineComponent != null)
+//                    {
+//                        outlineComponent.enabled = false;
+//                    }
+//                    highlightedObject = null;
+//                }
+//            }
+//        }
+//        else
+//        {
+//            if (repairText != null) repairText.SetActive(false);
+//            if (highlightedObject != null)
+//            {
+//                var outlineComponent = highlightedObject.GetComponent<Outline>();
+//                if (outlineComponent != null)
+//                {
+//                    outlineComponent.enabled = false;
+//                }
+//                highlightedObject = null;
+//            }
+//        }
 
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            //CloseRepairPanel();
-        }
-    }
+//        if (Input.GetKeyDown(KeyCode.Escape))
+//        {
+//            //CloseRepairPanel();
+//        }
+//    }
 
-    void HighlightObject(GameObject targetObject)
-    {
-        if (highlightedObject != targetObject)
-        {
-            if (highlightedObject != null)
-            {
-                var previousOutline = highlightedObject.GetComponent<Outline>();
-                if (previousOutline != null)
-                {
-                    previousOutline.enabled = false;
-                }
-            }
+//    void HighlightObject(GameObject targetObject)
+//    {
+//        if (highlightedObject != targetObject)
+//        {
+//            if (highlightedObject != null)
+//            {
+//                var previousOutline = highlightedObject.GetComponent<Outline>();
+//                if (previousOutline != null)
+//                {
+//                    previousOutline.enabled = false;
+//                }
+//            }
 
-            var currentOutline = targetObject.GetComponent<Outline>();
-            if (currentOutline != null)
-            {
-                currentOutline.enabled = true;
-            }
-            highlightedObject = targetObject;
-        }
-    }
+//            var currentOutline = targetObject.GetComponent<Outline>();
+//            if (currentOutline != null)
+//            {
+//                currentOutline.enabled = true;
+//            }
+//            highlightedObject = targetObject;
+//        }
+//    }
 
-    bool IsBrokenObject(GameObject targetObject)
-{
-    return targetObject.CompareTag("Broken");
-}
+//    bool IsBrokenObject(GameObject targetObject)
+//{
+//    return targetObject.CompareTag("Broken");
+//}
 
-    /*public void OpenRepairPanel()
-    {
-        if (!repairMiniGamePanel.activeSelf)
-        {
-            repairMiniGamePanel.SetActive(true);
-        }
-    }
+//    /*public void OpenRepairPanel()
+//    {
+//        if (!repairMiniGamePanel.activeSelf)
+//        {
+//            repairMiniGamePanel.SetActive(true);
+//        }
+//    }
 
-    public void CloseRepairPanel()
-    {
-        if (repairMiniGamePanel.activeSelf)
-        {
-            repairMiniGamePanel.SetActive(false);
-        }
-    }*/
-}
+//    public void CloseRepairPanel()
+//    {
+//        if (repairMiniGamePanel.activeSelf)
+//        {
+//            repairMiniGamePanel.SetActive(false);
+//        }
+//    }*/
+//}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,147 +1,144 @@
-using JetBrains.Annotations;
-using Mirror;
-using Mirror.BouncyCastle.Bcpg.Sig;
-using Mirror.BouncyCastle.Tls;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.ShaderGraph.Internal;
-using UnityEngine;
-using UnityEngine.UIElements;
-using UnityEngine.Animations;
-using Mirror.Examples.Billiards;
+//using JetBrains.Annotations;
+//using Mirror;
+//using Mirror.BouncyCastle.Bcpg.Sig;
+//using Mirror.BouncyCastle.Tls;
+//using System.Collections;
+//using UnityEngine;
+//using UnityEngine.UIElements;
+//using UnityEngine.Animations;
+//using Mirror.Examples.Billiards;
 
-public class PlayerController : NetworkBehaviour
-{
-    public Rigidbody _rb;
-    public CapsuleCollider _cb;
+//public class PlayerController : NetworkBehaviour
+//{
+//    public Rigidbody _rb;
+//    public CapsuleCollider _cb;
 
 
-    [Header ("Speed")]
-    public float moveSpeed;
-    public float walkSpeed;
-    public float crouchSpeed;
-    public float sprintSpeed;
+//    [Header ("Speed")]
+//    public float moveSpeed;
+//    public float walkSpeed;
+//    public float crouchSpeed;
+//    public float sprintSpeed;
 
-    [Space(25)]
+//    [Space(25)]
 
-    [Header ("Sprint")]
-    public float runningTime;
-    public float maxRunningTime;
-    public float minRunningTime;
-    public bool EnSprint;
+//    [Header ("Sprint")]
+//    public float runningTime;
+//    public float maxRunningTime;
+//    public float minRunningTime;
+//    public bool EnSprint;
 
-    [Space(25)]
+//    [Space(25)]
 
-    [Header ("Crouch")]
-    public float t;
-    public float crouchYScale;
-    private float startYScale;
-    public CapsuleCollider _cbCrouch;
-    public GameObject camCrouch;
-    public GameObject camHold;
-    Vector3 camStandPosition;
-    float colliderStandHeight;
+//    [Header ("Crouch")]
+//    public float t;
+//    public float crouchYScale;
+//    private float startYScale;
+//    public CapsuleCollider _cbCrouch;
+//    public GameObject camCrouch;
+//    public GameObject camHold;
+//    Vector3 camStandPosition;
+//    float colliderStandHeight;
 
-    [Space(25)]
+//    [Space(25)]
 
-    [Header ("KeyCode")]
-    public KeyCode jumpKey = KeyCode.Space;
-    public KeyCode sprintKey = KeyCode.LeftShift;
-    public KeyCode crouchKey = KeyCode.LeftControl;
+//    [Header ("KeyCode")]
+//    public KeyCode jumpKey = KeyCode.Space;
+//    public KeyCode sprintKey = KeyCode.LeftShift;
+//    public KeyCode crouchKey = KeyCode.LeftControl;
 
 
 
-    void Start()
-    {
-        moveSpeed = walkSpeed;
-        maxRunningTime = runningTime;
-        camStandPosition = camHold.transform.localPosition;
-        colliderStandHeight = _cb.height;
-        startYScale = transform.localScale.y;
-    }
-    private void FixedUpdate() //Déplacements
-    {
-        _rb.velocity = transform.right * Input.GetAxis("Horizontal") * moveSpeed +
-        transform.up * _rb.velocity.y +
-        transform.forward * Input.GetAxis("Vertical") * moveSpeed;
-    }
+//    void Start()
+//    {
+//        moveSpeed = walkSpeed;
+//        maxRunningTime = runningTime;
+//        camStandPosition = camHold.transform.localPosition;
+//        colliderStandHeight = _cb.height;
+//        startYScale = transform.localScale.y;
+//    }
+//    private void FixedUpdate() //Déplacements
+//    {
+//        _rb.velocity = transform.right * Input.GetAxis("Horizontal") * moveSpeed +
+//        transform.up * _rb.velocity.y +
+//        transform.forward * Input.GetAxis("Vertical") * moveSpeed;
+//    }
 
 
-    void Update()
-    {
-        //LE CROUCH
-        if (Input.GetKeyDown(crouchKey))
-        {
-            transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
-            _rb.AddForce(Vector3.down * 2f, ForceMode.Impulse);
-            camHold.transform.localPosition = Vector3.Lerp(camHold.transform.localPosition, camCrouch.transform.localPosition, t);
-            _cb.height = _cbCrouch.height;
-            moveSpeed = crouchSpeed;
-            //ajout anim accroupis
-        }
-        else if (Input.GetKeyUp(crouchKey)) //Se relever
-        {
-            RaycastHit hit;
-            if (Physics.Raycast(transform.position, transform.up, out hit, 2f)) //Ne peut pas se relever car plafond trop bas
-            {
-                camHold.transform.localPosition = camCrouch.transform.localPosition;
-                _cb.height = _cbCrouch.height;
-                moveSpeed = crouchSpeed;
-            }
-            else //Peut se relever
-            {
-                transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
-                camHold.transform.localPosition = Vector3.Lerp(camHold.transform.localPosition, camStandPosition, t);
-                _cb.height = colliderStandHeight;
-                moveSpeed = walkSpeed;
-                //ajout anim debout
-            }
-        }
+//    void Update()
+//    {
+//        //LE CROUCH
+//        if (Input.GetKeyDown(crouchKey))
+//        {
+//            transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
+//            _rb.AddForce(Vector3.down * 2f, ForceMode.Impulse);
+//            camHold.transform.localPosition = Vector3.Lerp(camHold.transform.localPosition, camCrouch.transform.localPosition, t);
+//            _cb.height = _cbCrouch.height;
+//            moveSpeed = crouchSpeed;
+//            //ajout anim accroupis
+//        }
+//        else if (Input.GetKeyUp(crouchKey)) //Se relever
+//        {
+//            RaycastHit hit;
+//            if (Physics.Raycast(transform.position, transform.up, out hit, 2f)) //Ne peut pas se relever car plafond trop bas
+//            {
+//                camHold.transform.localPosition = camCrouch.transform.localPosition;
+//                _cb.height = _cbCrouch.height;
+//                moveSpeed = crouchSpeed;
+//            }
+//            else //Peut se relever
+//            {
+//                transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
+//                camHold.transform.localPosition = Vector3.Lerp(camHold.transform.localPosition, camStandPosition, t);
+//                _cb.height = colliderStandHeight;
+//                moveSpeed = walkSpeed;
+//                //ajout anim debout
+//            }
+//        }
 
-        //SPRINT
-        if (Input.GetKeyDown(sprintKey) && moveSpeed == walkSpeed)
-        {
-            StartCoroutine(DeloadSprint());
-        }
-        if (Input.GetKeyUp(sprintKey))
-        {
-            StopCoroutine(DeloadSprint());
-            moveSpeed = walkSpeed;
-            EnSprint = false;
-            StartCoroutine(ReloadSprint());
-        }
-    }
+//        //SPRINT
+//        if (Input.GetKeyDown(sprintKey) && moveSpeed == walkSpeed)
+//        {
+//            StartCoroutine(DeloadSprint());
+//        }
+//        if (Input.GetKeyUp(sprintKey))
+//        {
+//            StopCoroutine(DeloadSprint());
+//            moveSpeed = walkSpeed;
+//            EnSprint = false;
+//            StartCoroutine(ReloadSprint());
+//        }
+//    }
 
     
 
 
 
-    //Reload et Deload du - SPRINT
-    IEnumerator DeloadSprint() 
-    {
-        EnSprint = true;
-        {
-            while(EnSprint)
-            {
-                moveSpeed = sprintSpeed;
-                runningTime -= Time.deltaTime;
-                if (runningTime <= minRunningTime)
-                {
-                    moveSpeed = walkSpeed;
-                    EnSprint = false;
-                }
-                Debug.Log("EnSprint");
-                yield return null;
-            }
-        }
-    }
-    IEnumerator ReloadSprint()
-    {
-        while(moveSpeed == walkSpeed && runningTime < maxRunningTime)
-        {
-            runningTime += Time.deltaTime;
-            yield return null;
-        }
-    }
-}
+//    //Reload et Deload du - SPRINT
+//    IEnumerator DeloadSprint() 
+//    {
+//        EnSprint = true;
+//        {
+//            while(EnSprint)
+//            {
+//                moveSpeed = sprintSpeed;
+//                runningTime -= Time.deltaTime;
+//                if (runningTime <= minRunningTime)
+//                {
+//                    moveSpeed = walkSpeed;
+//                    EnSprint = false;
+//                }
+//                Debug.Log("EnSprint");
+//                yield return null;
+//            }
+//        }
+//    }
+//    IEnumerator ReloadSprint()
+//    {
+//        while(moveSpeed == walkSpeed && runningTime < maxRunningTime)
+//        {
+//            runningTime += Time.deltaTime;
+//            yield return null;
+//        }
+//    }
+//}
