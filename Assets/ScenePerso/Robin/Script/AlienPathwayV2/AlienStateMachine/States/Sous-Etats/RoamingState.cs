98cc8f9332733bd32f4b27be565a3af769530d65
Rob11005 <162438098+Rob11005@users.noreply.github.com>
2025-04-30 13:07:56 +0200

    chalagadou

---
+++ Assets/ScenePerso/Robin/Script/AlienPathwayV2/AlienStateMachine/States/Sous-Etats/RoamingState.cs
@@ -0,0 +1,24 @@
+public class RoamingState : PatrolState
+{
+    public RoamingState(Alien alien, AlienStateMachine stateMachine) : base(alien, stateMachine)
+    {
+
+    }
+
+    public override void EnterState()
+    {
+
+    }
+    public override void ExitState()
+    {
+
+    }
+    public override void FrameUpdate()
+    {
+
+    }
+    public override void PhysicsUpdate()
+    {
+
+    }
+}
